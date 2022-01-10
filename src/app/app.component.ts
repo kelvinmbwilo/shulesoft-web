9c3394667b56534f424b337b4b94b04c093a70ba
kelvin mbwilo <kelvinmbwilo@gmail.com>
2022-01-11 00:19:14 +0300

    adding theming options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'shulesoft-frontend-new';
+export class AppComponent implements OnInit{
+
+  toggleControl = new FormControl(false);
+  @HostBinding('class') className = 'light-theme';
+  title = 'shule-soft-front-end';
+
+  ngOnInit(): void {
+    this.toggleControl.valueChanges.subscribe((darkMode: any) => {
+      const darkClassName = 'darkMode';
+      this.className = darkMode ? 'dark-theme' : 'light-theme';
+    });
+  }
 }
